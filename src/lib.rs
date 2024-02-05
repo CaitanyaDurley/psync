d2d8a5c46a3c52946556ab27a50499e408971a1f
Caitanya Durley <caitanya.v.durley@gmail.com>
2024-02-05 20:46:32 +0000

    Secondary thread traverses directory

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 use std::error::Error;
 use std::path::{Path, PathBuf};
+use std::fs;
+use std::io;
+use std::sync::mpsc;
+use std::thread;
 pub use clap::Parser;
 
 #[derive(Parser)]
@@ -11,6 +15,60 @@ pub struct Cli {
     threads: u8,
 }
 
+struct CopyJob {
+    id: usize,
+    src: PathBuf,
+    dest: PathBuf,
+    size: Option<usize>,
+}
+
 pub fn run(args: Cli) -> Result<(), Box<dyn Error>> {
+    let to_copy = begin_traversal(args.src.clone());
+    // let (done_tx, done_rx) = mpsc::channel();
+    let mut traversing = true;
+    let mut copying = true;
+    let mut id = 0;
+    while traversing || copying {
+        match to_copy.try_recv() {
+            Ok(path) => {
+                let dest = args.dest.join(path.strip_prefix(&args.src)?);
+                let job = CopyJob {
+                    id,
+                    src: path,
+                    dest,
+                    size: None,
+                };
+                copy(job);
+                id += 1;
+            },
+            Err(mpsc::TryRecvError::Disconnected) => {traversing = false},
+            Err(mpsc::TryRecvError::Empty) => (),
+        }
+    }
+    Ok(())
+}
+
+fn copy(job: CopyJob) {
+    todo!()
+}
+
+fn begin_traversal(src: PathBuf) -> mpsc::Receiver<PathBuf> {
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || traverse(&src, &tx));
+    rx
+}
+
+fn traverse(dir: &Path, sender: &mpsc::Sender<PathBuf>) -> io::Result<()> {
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?.path();
+        if entry.is_dir() {
+            traverse(&entry, &sender)?;
+        } else {
+            if let Err(_) = sender.send(entry) {
+                // the receiver on the main thread has disconnected
+                panic!();
+            }
+        }
+    }
     Ok(())
 }
