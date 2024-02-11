2e09ec13e51fa04c2c8c712e91f1859cb93f0afc
Caitanya Durley <caitanya.v.durley@gmail.com>
2024-02-11 17:48:12 +0000

    Create directory traversal module with traverse iterator

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use std::io;
 use std::path::{Path, PathBuf};
 use std::sync::mpsc;
 use thread_pool::ThreadPool;
+use directory_traversal::CopyJob;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -14,12 +15,6 @@ pub struct Cli {
     dest: PathBuf,
     #[arg(short, long, default_value_t = 1)]
     threads: u8,
-}
-
-struct CopyJob {
-    src: PathBuf,
-    dest: PathBuf,
-    size: Option<usize>,
 }
 
 pub fn run(mut args: Cli) -> Result<(), Box<dyn Error>> {
@@ -71,31 +66,100 @@ fn begin_traversal(pool: &ThreadPool, src: PathBuf, dest: PathBuf) -> mpsc::Rece
     rx
 }
 
-/// Traverse a directory tree, sending file copy jobs down the sender
-/// and creating parent directories under dest as you go
-///
-/// # Parameters
-/// src - an existing directory in which to begin traversal
-/// dest - an existing directory in which to create any dir trees found within src
-/// sender - a channel for sending files which need copying
-fn traverse(src: &Path, dest: &Path, sender: &mpsc::Sender<CopyJob>) -> io::Result<()> {
-    for entry in fs::read_dir(src)? {
-        let entry = entry?.path();
-        let dest = dest.join(entry.file_name().unwrap());
-        if entry.is_dir() {
-            fs::create_dir(&dest)?;
-            traverse(&entry, &dest, &sender)?;
-        } else {
-            let job = CopyJob {
-                src: entry.to_path_buf(),
-                dest,
-                size: None,
-            };
-            if let Err(_) = sender.send(job) {
-                // the receiver on the main thread has disconnected
-                panic!();
-            }
+mod directory_traversal {
+    use std::io;
+    use std::path::{Path, PathBuf};
+    use std::fs;
+
+    pub struct CopyJob {
+        pub src: PathBuf,
+        pub dest: PathBuf,
+    }
+
+    /// Returns an iterator over the subtree of src, yielding CopyJob(s)
+    /// and creating parent directories under dest as it goes. Returns an
+    /// io::Error if a directory cannot be read/created (e.g. permissions)
+    /// or if an intermittent IO fault is encountered.
+    ///
+    /// # Parameters
+    /// src - an existing directory in which to begin traversal
+    /// dest - an existing directory in which to create any dir trees found within src
+    pub fn traverse(src: &Path, dest: &Path) -> CopyJobIterator {
+        let state = State {
+            src: src.to_path_buf(),
+            dest: dest.to_path_buf(),
+            src_entries: None,
+        };
+        CopyJobIterator {
+            stack: vec![state],
+            errored: false,
         }
     }
-    Ok(())
+
+    struct State {
+        src: PathBuf,
+        dest: PathBuf,
+        src_entries: Option<fs::ReadDir>,
+    }
+
+    pub struct CopyJobIterator {
+        stack: Vec<State>,
+        errored: bool,
+    }
+
+    impl CopyJobIterator {
+        fn wrap_error(&mut self, err: io::Error) -> Option<io::Result<CopyJob>> {
+            self.errored = true;
+            Some(Err(err))
+        }
+    }
+
+    impl Iterator for CopyJobIterator {
+        type Item = io::Result<CopyJob>;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            if self.errored || self.stack.len() == 0 {
+                return None
+            }
+            let state = self.stack.pop().unwrap();
+            let mut src_entries = match state.src_entries {
+                Some(entries) => entries,
+                None => {
+                    match fs::read_dir(&state.src) {
+                        Ok(entries) => entries,
+                        Err(e) => return self.wrap_error(e),
+                    }
+                },
+            };
+            while let Some(res) = src_entries.next() {
+                let entry = match res {
+                    Ok(entry) => entry.path(),
+                    Err(e) => return self.wrap_error(e),
+                };
+                let dest = state.dest.join(entry.file_name().unwrap());
+                if entry.is_dir() {
+                    if let Err(e) = fs::create_dir(&dest) {
+                        return self.wrap_error(e)
+                    };
+                    self.stack.push(State {
+                        src: entry,
+                        dest,
+                        src_entries: None
+                    });
+                } else {
+                    let job = CopyJob {
+                        src: entry,
+                        dest,
+                    };
+                    self.stack.push(State {
+                        src: state.src,
+                        dest: state.dest,
+                        src_entries: Some(src_entries),
+                    });
+                    return Some(Ok(job))
+                }
+            };
+            return self.next()
+        }
+    }
 }
