02f5e36d17674a719842c89203f79c5ed0588f1e
Caitanya Durley <caitanya.v.durley@gmail.com>
2024-02-13 21:51:44 +0000

    Stop main thread hanging after copy finished

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use std::fs;
 use std::io;
 use std::path::{Path, PathBuf};
 use std::sync::mpsc;
+use std::sync::Arc;
 use thread_pool::ThreadPool;
 use directory_traversal::CopyJob;
 
@@ -27,19 +28,26 @@ pub fn run(mut args: Cli) -> Result<(), Box<dyn Error>> {
     validate_args(&mut args)?;
     let pool = ThreadPool::new(args.threads.into());
     let (tx, rx) = mpsc::channel();
-    let traversal_sender = tx.clone();
+    let sender = Arc::new(tx);
+    let traversal_sender = Arc::clone(&sender);
     pool.run(move || begin_traversal(&args.src, &args.dest, traversal_sender));
+    let mut idle;
     loop {
-        match rx.recv() {
+        // idle is true iff traversal is done and there are no currently running copy jobs
+        idle = Arc::strong_count(&sender) == 1;
+        match rx.try_recv() {
             Ok(m) => match m {
                 Message::ToCopy(job) => {
-                    let copy_sender = tx.clone();
+                    let copy_sender = Arc::clone(&sender);
                     pool.run(move || copy(job, copy_sender))
                 },
                 Message::Copied(b) => println!("Copied {b} bytes"),
                 Message::Err(e) => return Err(Box::new(e)),
             },
-            Err(_) => break,
+            Err(_) => if idle {
+                // there's no messages on the channel and no more are coming
+                break
+            },
         }
     }
     Ok(())
@@ -66,14 +74,14 @@ fn validate_args(args: &mut Cli) -> Result<(), &str> {
     Ok(())
 }
 
-fn copy(job: CopyJob, sender: mpsc::Sender<Message>) {
+fn copy(job: CopyJob, sender: Arc<mpsc::Sender<Message>>) {
     sender.send(match fs::copy(&job.src, &job.dest) {
         Ok(b) => Message::Copied(b),
         Err(e) => Message::Err(e),
     }).unwrap();
 }
 
-fn begin_traversal(src: &Path, dest: &Path, sender: mpsc::Sender<Message>) {
+fn begin_traversal(src: &Path, dest: &Path, sender: Arc<mpsc::Sender<Message>>) {
     for job in directory_traversal::traverse(src, dest) {
         sender.send(match job {
             Ok(job) => Message::ToCopy(job),
