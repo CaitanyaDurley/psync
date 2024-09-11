cb2e615a8ccf61e7d712e76fc27d1e9af0482ece
Caitanya Durley <caitanya.v.durley@gmail.com>
2024-09-11 21:26:10 +0100

    replace chunk comparison with size and mtime comparison

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -1,47 +1,47 @@
 pub mod directory_traversal;
 
 pub use directory_traversal::CopyJob;
+use std::os::unix::fs::MetadataExt;
 use std::{fs, io};
-use std::io::Read;
 use std::path::Path;
-use std::collections::HashMap;
-
-// Block size in bytes to compare src and dest
-const BLOCK_SIZE: usize = 1024;
-// Min number of bytes for it to be deemed worthwhile comparing the contents of a file in src and dest
-const MIN_BYTES_FOR_SYNC: usize = 10 * BLOCK_SIZE;
 
 
 pub fn sync(job: CopyJob) -> io::Result<u64> {
-    if job.may_exist {
-        merge_unchecked(job)
-    } else {
-        copy(job)
-    }
+    match DestState::get(&job)? {
+        DestState::InSync => return Ok(0),
+        DestState::OutOfSync => fs::remove_file(&job.dest)?,
+        DestState::Missing => (),
+        DestState::IsDirectory => return Err(io::Error::new(io::ErrorKind::AlreadyExists, format!("Found directory with conflicting name: {}", job.dest.display()))),
+    };
+    copy(&job)
 }
 
+enum DestState {
+    InSync,
+    OutOfSync,
+    Missing,
+    IsDirectory,
+}
 
-// Entrypoint for a merge job. Calls copy if:
-// 1. dest doesn't exist
-// 1. dest is smaller than MIN_BYTES_FOR_SYNC (deletes dest first)
-//. # Errors
-// 1. If dest exists and is a dir
-// 1. If the metadata of dest could not be retrieved
-fn merge_unchecked(job: CopyJob) -> io::Result<u64> {
-    let dest_meta = match job.dest.symlink_metadata() {
-        Ok(x) => x,
-        // assume that dest doesn't exist - if false then we're just kicking the error can down the road
-        Err(_) => return copy(job),
-    };
-    if dest_meta.is_dir() {
-        return Err(io::Error::new(io::ErrorKind::AlreadyExists, format!("Found directory with conflicting name: {}", job.dest.display())))
-    };
-    // dest is either a symlink or a file
-    if dest_meta.len() < MIN_BYTES_FOR_SYNC as u64 {
-        fs::remove_file(&job.dest)?;
-        copy(job)
-    } else {
-        merge_checked(&job.src, &job.dest)
+impl DestState {
+    fn get(job: &CopyJob) -> io::Result<Self> {
+        if !job.may_exist {
+            return Ok(Self::Missing)
+        }
+        let src_meta = fs::symlink_metadata(&job.src)?;
+        let dest_meta = match fs::symlink_metadata(&job.dest) {
+            Ok(m) => m,
+            // assume that dest doesn't exist - if false then we're just kicking the error can down the road
+            Err(_) => return Ok(Self::Missing),
+        };
+        if dest_meta.is_dir() {
+            return Ok(Self::IsDirectory)
+        }
+        if src_meta.modified()? == dest_meta.modified()? && src_meta.size() == dest_meta.size() {
+            Ok(Self::InSync)
+        } else {
+            Ok(Self::OutOfSync)
+        }
     }
 }
 
@@ -50,7 +50,7 @@ fn merge_unchecked(job: CopyJob) -> io::Result<u64> {
 // 1. If the copy failed
 // 1. If the file is a symlink and dest already exists
 // 1. If dest exists and is a directory
-fn copy(job: CopyJob) -> io::Result<u64> {
+fn copy(job: &CopyJob) -> io::Result<u64> {
     if job.symlink {
         copy_symlink(&job.src, &job.dest).and(Ok(0))
     } else {
@@ -69,46 +69,4 @@ fn copy(job: CopyJob) -> io::Result<u64> {
 fn copy_symlink(src: &Path, dest: &Path) -> io::Result<()> {
     let target = fs::read_link(src)?;
     std::os::unix::fs::symlink(target, dest)
-}
-
-
-// ********* rsync style logic *********
-
-// Handles a merge of src into dest. Assumes:
-// 1. src and dest exist
-// 1. src and dest are files (not symlinks)
-fn merge_checked(src: &Path, dest: &Path) -> io::Result<u64> {
-    let dest_block_lookup = build_dest_block_lookup(dest)?;
-    Ok(0)
-}
-
-fn build_dest_block_lookup(dest: &Path) -> io::Result<HashMap<[u8; BLOCK_SIZE], usize>> {
-    let mut dest_file = fs::File::open(dest)?;
-    let mut buf = [0; BLOCK_SIZE];
-    let mut byte_counter = 0;
-    let mut block_counter: usize = 0;
-    let mut possible_eof = false;
-    let mut block_lookup = HashMap::new();
-    loop {
-        let res = dest_file.read(&mut buf[byte_counter..]);
-        if let Err(e) = res {
-            match e.kind() {
-                io::ErrorKind::Interrupted => continue,
-                _ => return Err(e)
-            }
-        };
-        let res = res.unwrap();
-        if possible_eof && res == 0 {
-            // two successive Ok(0) => EOF
-            break
-        }
-        possible_eof = res == 0;
-        byte_counter += res;
-        if byte_counter == BLOCK_SIZE {
-            block_lookup.insert(buf, block_counter);
-            byte_counter = 0;
-            block_counter += 1;
-        }
-    }
-    Ok(block_lookup)
 }
